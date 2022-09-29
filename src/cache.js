af92e6ba77fc5267991770387027924df048aee4
Aram-Osipyan <osipyan.aram2017@ya.ru>
2022-09-29 10:07:42 +0300

    add tests and complete tasks

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,44 @@
 class Cache{
+    constructor() {
+        this._cacheValues = new Map();
+        this._cacheValueCounter = new Map();
+    }
 
+    /**
+     *
+     * @param key {string}
+     * @param value {Object}
+     * @param hitNumber {number}
+     */
+    set(key, value, hitNumber= 1){
+        this._cacheValues.set(key, value);
+        this._cacheValueCounter.set(key, hitNumber);
+    }
+    /**
+     *
+     * @param key {string}
+     */
+    get(key){
+        const counter = this._cacheValueCounter.get(key);
+        if (counter <= 0){
+            return null;
+        }
+        this._cacheValueCounter.set(key, counter - 1);
+        return this._cacheValues.get(key);
+    }
+
+    /**
+     * @returns {Map}
+     */
+    getStatistics(){
+        const result = new Map();
+        for (let key of this._cacheValues.keys()){
+            const value = this._cacheValues.get(key);
+            const count = this._cacheValueCounter.get(key);
+            result.set(key, {value: value, count: count});
+        }
+        return result
+    }
 }
+
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,60 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+/**
+ *
+ * @param hitNumber {number}
+ * @param key {string}
+ * @param value {Object}
+ */
+const createDefaultCache = function (hitNumber= 1, key = 'test key', value = 'test value'){
+    const cache = new Cache();
+    cache.set(key, value, hitNumber);
+    return cache;
+
+}
+test('it successful getting and settings', () => {
+    const cache = createDefaultCache();
+    const key = 'test key';
+    const value = 'test value';
+    expect(cache.get(key)).toBe(value);
+});
+
+test('it fail hit count is down', () => {
+    const cache = createDefaultCache(2);
+    const key = 'test key';
+    const value = 'test value';
+    cache.get(key);
+    cache.get(key);
+    expect(cache.get(key)).toBeNull();
+});
+
+test('it fail hit count by default is down', () => {
+    const cache = createDefaultCache(2);
+    const key = 'test key';
+    const value = 'test value';
+    cache.set(key, value);
+    cache.get(key);
+    expect(cache.get(key)).toBeNull();
+});
+
+test('get ', () => {
+    const cache = new Cache();
+    const key = 'test key';
+    const value = 'test value';
+    cache.set(key, value,3);
+    cache.get(key);
+    cache.get(key);
+    expect(cache.get(key)).toBe('test value');
+});
+
+test('test statistics ', () => {
+    const cache = new Cache();
+    const key = 'test key';
+    const value = 'test value';
+    const count = 3;
+    cache.set(key, value,count);
+
+    const statistics = cache.getStatistics();
+    expect(statistics.get(key).value).toBe(value);
+    expect(statistics.get(key).count).toBe(count);
 });
